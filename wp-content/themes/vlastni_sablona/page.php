d3627682453995e001dc41b774f5576bea0fabbf
Daryna Toporovska <Daryna.Toporovska@student.souepl.cz>
2025-09-25 11:32:07 +0200

    oprava

--- wp-content/themes/vlastni_sablona/page.php
+++ wp-content/themes/vlastni_sablona/page.php
@@ -1,9 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>
+<head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 
+        <?php 
+            if ( is_front_page() ) { // Pokud je to domovská stránka (statická stránka)
+                echo get_the_title( get_option( 'page_on_front' ) ); // Získá název statické domovské stránky
+            } else {
+                wp_title( '|', true, 'right' ); // Standardní titulek pro ostatní stránky
+            } 
+        ?>
+    </title>
+</head>
+<body>
+    <?php
 
-get_header();
-?>
+    get_header();
+    ?>
 
-<main id="primary" class="site-main">
+    <main id="primary" class="site-main">
     <?php
     while (have_posts()) :
         the_post();
@@ -24,11 +40,16 @@ get_header();
                 ?>
             </div>
         </article>
-    <?php
-    endwhile;
-    ?>
-</main>
 
-<?php
-get_footer();
-?>
+        <?php
+        endwhile;
+        ?>
+    </main>
+
+    <?php
+    get_footer();
+    ?>
+
+</body>
+</html>
+
